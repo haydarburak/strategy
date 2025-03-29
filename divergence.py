f019eff18205c1efcd05b493c253605f66f4cf63
haydarburak <burakagridag2@gmail.com>
2025-03-29 15:19:42 +0100

    divergence

--- divergence.py
+++ divergence.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def find_rsi_divergence(df, min_bars=30):
     df = df.copy()
 
@@ -11,35 +10,32 @@ def find_rsi_divergence(df, min_bars=30):
     df['Bearish_Divergence'] = np.nan
     df['Bullish_Divergence'] = np.nan
 
-    for i in range(min_bars, len(df)):
-        # Previous highs and lows
-        past_high = df.iloc[i - min_bars:i]['Close'].max()
-        past_low = df.iloc[i - min_bars:i]['Close'].min()
+    # Previous highs and lows
+    past_high = df.iloc[len(df) - min_bars -1:len(df)-1]['Close'].max()
+    past_low = df.iloc[len(df) - min_bars -1:len(df)-1]['Close'].min()
 
-        past_rsi_high = df.iloc[i - min_bars:i]['RSI'].max()
-        past_rsi_low = df.iloc[i - min_bars:i]['RSI'].min()
+    past_rsi_high = df.iloc[len(df) - min_bars -1:len(df)-1]['RSI'].max()
+    past_rsi_low = df.iloc[len(df) - min_bars -1:len(df)-1]['RSI'].min()
 
-        # Current price and RSI
-        curr_price = df.iloc[i]['Close']
-        curr_rsi = df.iloc[i]['RSI']
+    # Current price and RSI
+    curr_price = df.iloc[-1]['Close']
+    curr_rsi = df.iloc[-1]['RSI']
 
-        # Bearish divergence (price higher high, RSI lower high)
-        if past_rsi_high > 70 and curr_price > past_high and curr_rsi < past_rsi_high:
-            df.at[df.index[i], 'Bearish_Divergence'] = curr_price
-        else:
-            df.at[df.index[i], 'Bearish_Divergence'] = 0
+    # Bearish divergence (price higher high, RSI lower high)
+    if past_rsi_high > 70 and curr_price > past_high and curr_rsi < past_rsi_high:
+        df.at[df.index[-1], 'Bearish_Divergence'] = curr_price
+    else:
+        df.at[df.index[-1], 'Bearish_Divergence'] = 0
 
-
-        # Bullish divergence (price lower low, RSI higher low)
-        if past_rsi_low < 30 and curr_price < past_low and curr_rsi > past_rsi_low:
-            df.at[df.index[i], 'Bullish_Divergence'] = curr_price
-        else:
-            df.at[df.index[i], 'Bullish_Divergence'] = 0
+    # Bullish divergence (price lower low, RSI higher low)
+    if past_rsi_low < 30 and curr_price < past_low and curr_rsi > past_rsi_low:
+        df.at[df.index[-1], 'Bullish_Divergence'] = curr_price
+    else:
+        df.at[df.index[-1], 'Bullish_Divergence'] = 0
 
 #    plot_rsi_divergence(df)
 
     return df
-
 
 def plot_rsi_divergence(df):
     """
